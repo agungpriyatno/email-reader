f3478ee57bfa38f160f4154519f982a1396843e6
Agung Priyatno <agungpriyatno303@gmail.com>
2024-04-28 16:23:40 +0700

    update

--- src/app/backoffice/page.tsx
+++ src/app/backoffice/page.tsx
@@ -6,9 +6,9 @@ export default function Home() {
     <main className="h-screen w-full relative">
       <section className=" absolute left-0 top-0 w-full h-full z-10">
         <div className="h-full w-full flex flex-col justify-center place-items-center space-y-3">
-          <h1 className=" text-3xl font-bold">NAMA USAHA LU</h1>
+          <h1 className=" text-3xl font-bold">BACKOFFICE</h1>
           <Button asChild>
-            <Link href={"/signin"}>MASUK</Link>
+            <Link href={"/backoffice/signin"}>MASUK</Link>
           </Button>
         </div>
       </section>
