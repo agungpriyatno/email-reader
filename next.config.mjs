5a39adf3caf15d3ca8754bcbc187bd8742f14181
Agung Priyatno <agungpriyatno303@gmail.com>
2024-05-02 17:11:20 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
+    experimental: {
+        serverActions: {
+            allowedOrigins: [
+                'http://localhost'
+            ]
+        }
+    }
 };
 
 export default nextConfig;
